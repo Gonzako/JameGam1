02344d25cb1f3c318d829b2321458fbdb421eee4
Gonzako <gonzako123@gmail.com>
2022-01-25 10:30:38 +0100

    Added basic player controller + movement

---
+++ Assets/Scripts/PlayerController/PlayerMovement.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    AnimationCurve MovCurve;
+    Rigidbody2D rb;
+    [SerializeField]
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontalInput = (Input.GetAxis("Horizontal"));
+        var verticalInput = (Input.GetAxis("Vertical"));
+        rb.velocity =( new Vector2(MovCurve.Evaluate(horizontalInput)*Mathf.Sign(horizontalInput), MovCurve.Evaluate(verticalInput)*Mathf.Sign(verticalInput)) * speed);
+    }
+}
