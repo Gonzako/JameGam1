a3a6cfe3dd90aab1c6e0d051344e3552ebd97566
realTobby <tkattanek00@gmail.com>
2022-01-26 09:55:19 +0100

    Parallax direction changed

--- Assets/Scripts/Parallax/ParallaxLayer.cs
+++ Assets/Scripts/Parallax/ParallaxLayer.cs
@@ -9,7 +9,7 @@ public class ParallaxLayer : MonoBehaviour
     public void Move(float delta)
     {
         Vector3 newPos = transform.localPosition;
-        newPos.x -= delta * parallaxFactor;
+        newPos.x += delta * parallaxFactor;
         transform.localPosition = newPos;
     }
 }
