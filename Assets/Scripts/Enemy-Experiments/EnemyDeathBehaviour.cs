ab95607fcd2f98839494ed48c5bcdf7f689fdb74
MaxTheEternal <98349129+MaxTheEternal@users.noreply.github.com>
2022-01-27 22:11:03 +0100

    Particles on Emeny Deaths
    a simple particle spray when enemies die. made it a prefab.

--- Assets/Scripts/Enemy-Experiments/EnemyDeathBehaviour.cs
+++ Assets/Scripts/Enemy-Experiments/EnemyDeathBehaviour.cs
@@ -5,22 +5,8 @@ using UnityEngine;
 public class EnemyDeathBehaviour : MonoBehaviour
 {
     public int Health = 5;
+    public GameObject deathParticle;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if(Health <= 0)
-        {
-            GameObject.FindGameObjectWithTag("World").GetComponent<MainWorld>().EnemyKilled();
-            Destroy(this.transform.parent.gameObject);
-        }
-    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -28,8 +14,14 @@ public class EnemyDeathBehaviour : MonoBehaviour
         {
             Health -= 1;
 
-            
+
             this.GetComponent<AudioSource>().Play();
+        }
+        if (Health <= 0)
+        {
+            Instantiate(deathParticle, transform.position, Quaternion.identity);
+            GameObject.FindGameObjectWithTag("World").GetComponent<MainWorld>().EnemyKilled();
+            Destroy(this.transform.parent.gameObject);
         }
     }
 }
